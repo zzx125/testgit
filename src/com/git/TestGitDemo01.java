35d5deb8541405ad99db4c86f7ed237e74a5a885
1209002651@qq.com <1209006251@qq.com>
2021-05-14 22:09:51 +0800

    将项目提交到版本库

---
+++ src/com/git/TestGitDemo01.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class TestGitDemo01 {
+    public static void main(String[] args) {
+        System.out.println("这是TestGitDemo01");
+        System.out.println("我修改了TestGitDemo01");
+    }
+    public void add(){
+        System.out.println("这是在student001分支上新增方法");
+    }
+}
---
+++ src/com/git/TestGitDemo02.java
@@ -0,0 +1,7 @@
+package com.git;
+
+public class TestGitDemo02 {
+    public static void main(String[] args) {
+        System.out.println("这是TestGitDemo02");
+    }
+}
